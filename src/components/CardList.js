3646ba46ff949af4ab7acdc2f3fa5417bd9367d4
Kevinn89 <mitchell.kevinn@gmail.com>
2018-12-06 23:00:42 -0600

    first commit

---
+++ src/components/CardList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './Card.js';
+
+const CardList = ({robots}) =>
+{
+	const list = robots.map((user,i) => {
+
+		return <Card key ={i} id = {robots[i].id} name={robots[i].name} email = {robots[i].email}/>
+
+	});
+
+return(
+	<div>
+			{list}
+	</div>);
+
+}
+export default CardList;
